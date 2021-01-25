976f8c82623f571751e6912fd379442b9312fcc7
freesgen <jesusant.guerrero@gmail.com>
2021-01-25 12:46:33 -0400

    Install tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+<div class="text-center">
+  <img alt="Vue logo" src="./assets/logo.png" class="mx-auto" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
+</div>
 </template>
 
 <script setup>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,13 @@
 <template>
-  <h1>{{ msg }}</h1>
-
+<div class="mx-10">
+  <h1 class="text-red-500">{{ msg }}</h1>
   <p>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
   </p>
 
-  <button @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+</div>
+
 </template>
 
 <script setup>
