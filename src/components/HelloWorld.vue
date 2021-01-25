6d2405fa10722ac695dbe414254de031ded94b5f
freesgen <jesusant.guerrero@gmail.com>
2021-01-25 15:23:42 -0400

    Add styles to line up

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,11 +8,11 @@
   <div class="text-left flex">
     <div class="zen__view w-8/12 mr-5">
       <header class="flex justify-between mb-5"> 
-        <h1> Task Title</h1>
-        <div> 25:00 </div>
+        <h1 class="text-2xl font-bold text-blue-500"> Task Title</h1>
+        <div class="font-bold text-2xl"> 25:00 </div>
       </header>
 
-      <div>
+      <div class="mt-4 py-2">
         <div class="zen__datails">
           <div class="task__description mb-4">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error atque, molestiae beatae est assumenda ipsam explicabo corporis? Facilis suscipit quo corrupti animi amet ad illo ea! Nemo cupiditate expedita recusandae.
@@ -49,31 +49,36 @@
 
     <div class="zen__comming-up w-4/12 ml-5">
       <header class="text-right mb-5">
-        <h1> Line Up</h1>
+        <h1 class="text-2xl font-bold text-blue-500"> Line Up</h1>
         <div class="task-new">
 
         </div>
       </header>
 
-      <div class="comming-up__list">
+      <div class="comming-up__list divide-y-2 divide-gray-200 divide-dashed">
         <div class="quick__add mb-5">
-          <h4>Quick Add</h4>
-          <div class="task-new">
-            Click to add a quick task
-          </div>
+          <h4 class="font-bold">Quick Add</h4>
+          <quick-add></quick-add>
         </div>
-        <div class="commig-up__today">
-          <h4> Todo</h4>
-          <div class="" v-for="i in [1,2,3,4,5]" :key="i">
-            <h4> Task {{ i }}</h4>
-          </div>
+        <div class="commig-up__today mt-6 py-3">
+          <h4 class="mb-2 font-bold"> Todo</h4>
+          <TaskItem 
+            v-for="task in [1,2,3,4,5]" 
+            :key="task" 
+            :task="task"
+          >
+          </TaskItem>
         </div>
 
-        <div class="comming-up__scheduled mt-5">
-          <h4> Scheduled</h4>
-          <div class="" v-for="i in [1,2,3]" :key="i">
-            <h4> Task {{ i }}</h4>
-          </div>
+        <div class="comming-up__scheduled mt-6 py-3">
+          <h4 class="mb-2 font-bold"> Scheduled</h4>
+           <TaskItem 
+            class="opacity-60 hover:opacity-100"
+            v-for="task in [1,2,3]" 
+            :key="task" 
+            :task="task"
+          >
+          </TaskItem>
         </div>
       </div>
     </div>
@@ -85,6 +90,8 @@
 
 <script setup>
 import { defineProps, reactive } from 'vue'
+import TaskItem from "./TaskItem.vue"
+import QuickAdd from "./QuickAdd.vue"
 
 defineProps({
   msg: String
---
+++ src/components/QuickAdd.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="task-item flex justify-between mb-2 shadow-md border-gray-200 border-2 px-4 py-3 rounded-md items-center cursor-default">
+    <div class="flex items-center">
+      <div class="mx-3 rounded-md bg-blue-100 px-2 py-1 text-blue-500"> 
+          <i class="fa fa-plus"></i>
+      </div>
+      <input type="text" class="focus:outline-none" placeholder="Add quick task">
+    </div>
+    <div class="task-item__controls flex">
+      <div class="mx-2">
+        <i class="fa fa-clock"></i>
+      </div>
+      <div class="mx-2">
+        <i class="fa fa-tags cursor-pointer"></i>
+      </div>
+      <div class="mx-2">
+          <i class="fa fa-ellipsis-v cursor-pointer"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue"
+
+defineProps({
+    task: String
+})
+</script>
