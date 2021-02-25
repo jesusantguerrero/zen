f4dc205d3ee702ba55962dcad9aeade3f3ba3d2e
freesgen <jesusant.guerrero@gmail.com>
2021-02-25 12:03:30 -0400

    Draft landing page

---
+++ src/views/landing/Features.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="py-20 bg-gray-50">
+  <div class="container items-center max-w-6xl px-4 px-10 mx-auto sm:px-20 md:px-32 lg:px-16">
+    <div class="flex flex-wrap items-center -mx-3">
+      <div class="order-1 w-full px-3 lg:w-1/2 lg:order-0">
+        <div class="w-full lg:max-w-md">
+          <h2 class="mb-4 text-3xl font-bold leading-tight tracking-tight sm:text-4xl font-heading">Jam-packed with all the tools you need to succeed!</h2>
+          <p class="mb-4 font-medium tracking-tight text-gray-400 xl:mb-6">It's never been easier to build a business of your own. Our tools will help you with the following:</p>
+          <ul>
+            <li class="flex items-center py-2 space-x-4 xl:py-3">
+              <svg class="w-8 h-8 text-pink-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path></svg>
+              <span class="font-medium text-gray-500">Tasks with checklists.</span>
+            </li>
+            <li class="flex items-center py-2 space-x-4 xl:py-3">
+              <svg class="w-8 h-8 text-yellow-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path></svg>
+              <span class="font-medium text-gray-500">Promodoro + TimeTracker in One.</span>
+            </li>
+            <li class="flex items-center py-2 space-x-4 xl:py-3">
+              <svg class="w-8 h-8 text-yellow-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path></svg>
+              <span class="font-medium text-gray-500">Stats.</span>
+            </li>
+            <li class="flex items-center py-2 space-x-4 xl:py-3">
+              <svg class="w-8 h-8 text-yellow-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path></svg>
+              <span class="font-medium text-gray-500">Promodoro + TimeTracker in One.</span>
+            </li>
+            <li class="flex items-center py-2 space-x-4 xl:py-3">
+              <svg class="w-8 h-8 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path></svg>
+              <span class="font-medium text-gray-500">Eisenhower Matrix.</span>
+            </li>
+            <li class="flex items-center py-2 space-x-4 xl:py-3">
+              <svg class="w-8 h-8 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path></svg>
+              <span class="font-medium text-gray-500">Mobile friendly.</span>
+            </li>
+            <li class="flex items-center py-2 space-x-4 xl:py-3">
+              <svg class="w-8 h-8 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path></svg>
+              <span class="font-medium text-gray-500">Realtime.</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="w-full px-3 mb-12 lg:w-1/2 order-0 lg:order-1 lg:mb-0"><img class="mx-auto sm:max-w-sm lg:max-w-full" src="https://cdn.devdojo.com/images/november2020/feature-graphic.png" alt="feature image"></div>
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/landing/Hero.vue
@@ -0,0 +1,71 @@
+<template>
+    <header class="w-full antialiased bg-gray-900 select-none relative">
+        <div class="mx-auto max-w-7xl z-50">
+            <nav class="flex items-center w-full h-24" x-data="{ showMenu: false }">
+                <div class="relative flex flex-wrap items-center justify-between w-full h-24 mx-auto font-medium md:justify-center">
+                    <a href="#_" class="w-1/4 py-4 pl-6 pr-4 md:pl-4 md:py-0 z-index">
+                        <span class="text-xl font-black leading-none text-white select-none logo">Zenboard<span class="text-green-500">.</span></span>
+                    </a>
+                    <div class="fixed top-0 left-0 z-40 items-center hidden w-full h-full p-3 text-xl bg-gray-900 bg-opacity-50 md:text-sm lg:text-base md:w-3/4 md:bg-transparent md:p-0 md:relative md:flex" :class="{'flex': showMenu, 'hidden': !showMenu }">
+                        <div class="flex-col w-full h-auto h-full overflow-hidden bg-white rounded-lg select-none md:bg-transparent md:rounded-none md:relative md:flex md:flex-row md:overflow-auto">
+                            <div class="flex flex-col items-center justify-center w-full h-full mt-12 text-center text-green-700 md:text-green-200 md:w-2/3 md:mt-0 md:flex-row md:items-center">
+                                <a href="#" class="inline-block px-4 py-2 mx-2 font-medium text-left text-green-700 md:text-white md:px-0 lg:mx-3 md:text-center">Home</a>
+                                <a href="#" class="inline-block px-0 px-4 py-2 mx-2 font-medium text-left md:px-0 hover:text-green-800 md:hover:text-white lg:mx-3 md:text-center">Features</a>
+                                <a href="#" class="inline-block px-0 px-4 py-2 mx-2 font-medium text-left md:px-0 hover:text-green-800 md:hover:text-white lg:mx-3 md:text-center">Blog</a>
+                                <a href="#" class="inline-block px-0 px-4 py-2 mx-2 font-medium text-left md:px-0 hover:text-green-800 md:hover:text-white lg:mx-3 md:text-center">Contact</a>
+                            </div>
+                            <div class="flex flex-col items-center justify-end w-full h-full pt-4 md:w-1/3 md:flex-row md:py-0">
+                                <a href="/login" class="w-full pl-6 mr-0 text-green-200 hover:text-white md:pl-0 md:mr-3 lg:mr-5 md:w-auto">Sign In</a>
+                                <a href="/register" class="inline-flex items-center justify-center px-4 py-2 mr-1 text-base font-medium leading-6 text-green-600 whitespace-no-wrap transition duration-150 ease-in-out bg-green-100 border border-transparent rounded-full hover:bg-white focus:outline-none focus:border-green-700 focus:shadow-outline-green active:bg-green-700">Sign Up</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div @click="showMenu = !showMenu" class="absolute right-0 z-50 flex flex-col items-end w-10 h-10 p-2 mr-4 rounded-full cursor-pointer md:hidden" :class="{ 'text-green-700 hover:bg-gray-200': showMenu, 'hover:bg-green-700 text-green-200': !showMenu }">
+                        <svg class="w-6 h-6" x-show="!showMenu" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" x-cloak>
+                            <path d="M4 6h16M4 12h16M4 18h16"></path>
+                        </svg>
+                        <svg class="w-6 h-6" x-show="showMenu" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" x-cloak>
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                        </svg>
+                    </div>
+                </div>
+            </nav>
+
+            <div class="container py-32 mx-auto text-center sm:px-4 z-50">
+                <h1 class="text-4xl font-extrabold leading-10 tracking-tight text-white sm:text-5xl sm:leading-none md:text-4xl xl:text-5xl"><span class="block">Keep yourself in the zone</span> <span class="relative inline-block mt-3 text-transparent text-white">and focus in the moment.</span></h1>
+                <div class="max-w-lg mx-auto mt-6 text-sm text-center text-green-200 md:mt-12 sm:text-base md:max-w-xl md:text-lg xl:text-xl">Take the step to improve your daily routine and get things done!</div>
+                <div class="relative flex items-center max-w-md mx-auto mt-12 text-center">
+                    <input type="text" name="email" placeholder="Email Address" class="w-full h-12 px-6 py-2 font-medium text-green-800 rounded-l-full focus:outline-none">
+                    <span class="relative top-0 right-0 block rounded-full shadow-sm">
+                        <button type="button" class="inline-flex items-center w-32 h-12 px-8 text-base font-bold leading-6 text-white transition duration-150 ease-in-out bg-green-400 border border-transparent rounded-r-full hover:bg-green-700 focus:outline-none focus:border-green-700 focus:shadow-outline-green active:bg-green-700">
+                            Sign Up
+                        </button>
+                    </span>
+                </div>
+                <div class="mt-8 text-sm text-green-300">By signing up, you agree to our terms and services.</div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+.background-header {
+    background: url('../../assets/zen-photo.jpg');
+    background-size: cover;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+
+    &::after {
+        content: "";
+        @apply bg-gray-800;
+        display: block;
+        position: absolute;
+        width: 100%;
+        top: 0;
+        opacity: .85;
+        left: 0;
+        height: 100%;
+    }
+}
+</style>
