da989ee98ef61d4b08d47e25ea6e6f97e2cb527d
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-05-14 17:19:32 -0400

    Add stories and time tracker wrapper

---
+++ src/stories/atoms/DateSelect.stories.js
@@ -0,0 +1,27 @@
+import DateSelect from '@atoms/DateSelect.vue';
+
+export default {
+  title: 'Atoms/DateSelect',
+  component: DateSelect,
+  argTypes: {},
+};
+
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { DateSelect },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    return { args };
+  },
+  // And then the `args` are bound to your component with `v-bind="args"`
+  template: `<div class="w-40">
+    <date-select v-bind="args" />
+  </div>
+  `,
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  value: "",
+  placeholder: "Select a date"
+};
